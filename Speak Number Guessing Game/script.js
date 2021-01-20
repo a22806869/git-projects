4f7bee69424acc9e0371584b97611466017f150f
chieh <a22806869@hotmail.com.tw>
2021-01-20 23:43:49 +0800

    finish reloade page btn

--- Speak Number Guessing Game/script.js
+++ Speak Number Guessing Game/script.js
@@ -55,7 +55,9 @@ function checkNumber(msg) {
 //這邊+1因為Math.random*100數字只到99不會到100所以必須做這個動作
 const randomNum = Math.floor(Math.random() * 100) + 1;
 
+//把答案呈現在頁面上以便確認功能
 console.log(randomNum);
+
 // 檢查隨機數字有無正常生成
 // console.log(randomNum);
 
@@ -71,5 +73,14 @@ recognition.start();
 //speak result
 recognition.addEventListener('result', onSpeak)
 
-//end SR service
+//當猜一次後(沒有答對)讓遊戲繼續
 recognition.addEventListener('end', () => recognition.start());
+
+//搭遊戲答對會出現reload按鈕設置事件click並且判斷其id是否為play-again是的話畫面reload
+document.body.addEventListener('click', (e) => {
+
+    // 因為這邊的id是只有答對後才出現所以抓取事件在整個body上面所以必須選取其中的target.id才有辦法明確的抓到這個按鈕
+    if (e.target.id == 'play-again') {
+        window.location.reload();
+    }
+})
