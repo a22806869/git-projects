518058d8bfd979ec3d5453bd612fd2bddb3be113
chieh <a22806869@hotmail.com.tw>
2021-01-23 19:02:50 +0800

    randomize the richest person list

--- SortableList/script.js
+++ SortableList/script.js
@@ -26,7 +26,15 @@ createList();
 function createList(){
 
     // 如果不使用展開運算子就會只印出一個陣列包含所有名字而無法迭代
-    [...richestPeoples].forEach((person, index)=>{
+    [...richestPeoples]
+    .map((a) =>({value:a, sort:Math.random()}))
+
+    // 這邊a.sort後面的sort部分是上面object的屬性不是方法要注意
+    .sort((a,b) =>(a.sort-b.sort))
+    .map((a) =>a.value)
+    .forEach((person, index)=>{
+        console.log(person);
+
         const listItem = document.createElement('li');
 
         listItem.setAttribute('data-index',index);
@@ -41,6 +49,7 @@ function createList(){
 
         listItems.push(listItem);
 
+        // 把剛剛做好的li內容貼到DOM上
         draggable_list.appendChild(listItem);
     })
 
