61431cd8943ec0f26ce54e32b9370bd50442b83f
chieh <a22806869@hotmail.com.tw>
2021-01-28 00:11:17 +0800

    finish func getUserData and ready to push them to DOM

--- githubfinder/script.js
+++ githubfinder/script.js
@@ -4,14 +4,14 @@ function getInputValue() {
     const searchUser = $('#searchUser');
 
     searchUser.keyup(function () {
-        getUserProfile($(this).val());
+        getUserData($(this).val());
     });
 }
 
 getInputValue();
 
-
-function getUserProfile() {
+// 把取得的input作為變數放到url內去搜尋每個輸入的使用者
+function getUserData() {
     let username = $('#searchUser').val();
     let url = `https://api.github.com/users/${username}`
     $.ajax({
@@ -19,7 +19,48 @@ function getUserProfile() {
         url: url,
         dataType: "json",
         success: function (res) {
-            console.log(res);
+            let obj = {};
+            obj.avatar_url = res.avatar_url;
+            obj.profile_url = res.html_url;
+            obj.public_gists = res.public_gists;
+            obj.public_repos = res.public_repos;
+            obj.followers = res.followers;
+            obj.following = res.following;
+            obj.comany = res.company;
+            obj.location = res.location;
+            obj.blog = res.blog;
+            obj.create_at = res.create_at;
+            // console.log(obj);
+            pushElToDOM(obj);
         }
+
     });
 };
+
+
+function pushElToDOM(obj) {
+
+    `<div id="profile">
+            <div class="card card-body mb-3">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img class="img-fluid mb-2" src="https://avatars.githubusercontent.com/u/13644380?v=4">
+                        <a href="https://github.com/Coding" target="_blank" class="btn btn-primary btn-block mb-4">View
+                            Profile</a>
+                    </div>
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public Repos: 72</span>
+                        <span class="badge badge-secondary">Public Gists: 0</span>
+                        <span class="badge badge-success">Followers: 0</span>
+                        <span class="badge badge-info">Following: 0</span>
+                        <br><br>
+                        <ul class="list-group">
+                            <li class="list-group-item">Company: null</li>
+                            <li class="list-group-item">Website/Blog: https://Coding.net</li>
+                            <li class="list-group-item">Location: In the Cloud</li>
+                            <li class="list-group-item">Member Since: 2015-08-04T14:33:39Z</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>`
+}
