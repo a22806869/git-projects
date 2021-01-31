733a20cd518e9c6bcfdde61f37eb278a62ae048f
chieh <a22806869@hotmail.com.tw>
2021-01-31 20:15:39 +0800

    finsih img display part depending on the POP status

--- showweather/script.js
+++ showweather/script.js
@@ -10,28 +10,39 @@ fetch(
     // console.log(data.records.location);
 
     [...weatherData].forEach((currentValue, index) => {
-        let name = weatherData[index].locationName;
-        let POP =
-          weatherData[index].weatherElement[1].time[2].parameter.parameterName;
-        let Wx =
-          weatherData[index].weatherElement[0].time[2].parameter.parameterName;
-        let MinT =
-          weatherData[index].weatherElement[2].time[2].parameter.parameterName;
-        let MaxT =
-          weatherData[index].weatherElement[4].time[2].parameter.parameterName;
-  
-      
-        console.log(currentValue);
+      let name = weatherData[index].locationName;
+      let POP =
+        weatherData[index].weatherElement[1].time[2].parameter.parameterName;
+      let Wx =
+        weatherData[index].weatherElement[0].time[2].parameter.parameterName;
+      let MinT =
+        weatherData[index].weatherElement[2].time[2].parameter.parameterName;
+      let MaxT =
+        weatherData[index].weatherElement[4].time[2].parameter.parameterName;
 
+      console.log(currentValue);
 
+      let img;
+      if (POP == 0) {
+        img = "img/sun.svg";
+      } else if (POP > 25 || POP < 40) {
+        img = "img/cloud-sun.svg";
+      } else if (POP > 50) {
+        img = "Um.svg";
+      } else {
+        img = "rain.svg";
+      }
 
-
-
-
-
-
-
-
-
-
+      let card = document.querySelector(".container");
+      card.innerHTML += `
+          
+      <div class = "weather">
+          <img src="${img}" alt="">
+          <p >location: ${name}</p>
+          <p >tmp: ${MinT}&#8451~${MaxT}&#8451</p>
+          <p >降雨機率: ${POP}%</p>
+          <p >${Wx}</p>
+      </div>
+      `;
+    });
   });
