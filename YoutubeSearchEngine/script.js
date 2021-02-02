b5e86341729a9b3adc3cdb9ee163ee2b4ecfced2
chieh <a22806869@hotmail.com.tw>
2021-02-02 21:47:05 +0800

    testing js part not finish yet

--- YoutubeSearchEngine/script.js
+++ YoutubeSearchEngine/script.js
@@ -2,10 +2,9 @@
 $("#btngetval").click(function (e) {
   e.preventDefault();
   let val = $("#inputval").val();
-  console.log(val);
+  // console.log(val);
 
   getVideoData(val);
-  nextPage(val);
 });
 
 function getVideoData(val) {
@@ -16,12 +15,16 @@ function getVideoData(val) {
     data: "data",
     dataType: "json",
     success: function (res) {
-      console.log(res);
+      // console.log(res);
 
-      console.log(res);
       let videos = res.items;
 
-      console.log([...videos]);
+      // console.log([...videos]);
+      console.log(res.nextPageToken);
+
+      let nextPageToken = res.nextPageToken;
+      let prevPageToken = res.prevPageToken;
+
 
       let output = "";
 
@@ -41,17 +44,142 @@ function getVideoData(val) {
                   <p>${vid.snippet.description}</p>
                 </div>
               </section>
-              
-              
-              
               `;
 
         console.log(output);
         $(".123").html(output);
 
-        let btn = `<button class="next-btn btn" onclick = nextPage();>Next Page</button>`;
-        $(".456").html(btn);
+        // nextPage(nextPageToken);
+
+        // // 加入下一頁按鈕
+        // let btn = `<button class="next-btn btn" onclick = "nextPage()";>Next Page</button>`;
+        let buttons = getButtons(nextPageToken, prevPageToken);
+        $(".456").html(buttons);
       });
     },
   });
+}
+
+// Next Page Function
+function nextPage() {
+  var token = $('#next-button').data('token');
+  var q = $('#next-button').data('query');
+
+  // Clear Results
+  $('.123').html('');
+  $('.456').html('');
+
+  // Get Form Input
+  q = $('#query').val();
+
+  // Run GET Request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+      part: 'snippet, id',
+      q: q,
+      pageToken: token,
+      type: 'video',
+      key: 'AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I'
+    },
+    function (data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      // Log Data
+      console.log(data);
+
+      [...data].forEach(function (vid) {
+        output += `<section class="video-area">
+                <div class="img">
+                  
+                  <a href="https://www.youtube.com/embed/${vid.id.videoId}"
+                    ><img src="${vid.snippet.thumbnails.default.url}"
+                  /></a>
+                </div>
+
+                <div class="text">
+                  <a href="https://www.youtube.com/embed/${vid.id.videoId}"
+                    ><h3>${vid.snippet.title}</h3></a
+                  ><small>By <span> ${vid.snippet.channelTitle} </span> on ${vid.snippet.publishedAt}</small>
+                  <p>${vid.snippet.description}</p>
+                </div>
+              </section>
+              `;
+
+        var buttons = getButtons(prevPageToken, nextPageToken);
+
+        // Display Buttons
+        $('.456').append(buttons);
+      });
+    })
+};
+
+// Prev Page Function
+function nextPage() {
+  var token = $('#prev-button').data('token');
+  var q = $('#prev-button').data('query');
+
+  // Clear Results
+  $('.123').html('');
+  $('.456').html('');
+
+  // Get Form Input
+  q = $('#query').val();
+
+  // Run GET Request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+      part: 'snippet, id',
+      q: q,
+      pageToken: token,
+      type: 'video',
+      key: 'AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I'
+    },
+    function (data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      // Log Data
+      console.log(data);
+
+      [...data].forEach(function (vid) {
+        output += `<section class="video-area">
+                <div class="img">
+                  
+                  <a href="https://www.youtube.com/embed/${vid.id.videoId}"
+                    ><img src="${vid.snippet.thumbnails.default.url}"
+                  /></a>
+                </div>
+
+                <div class="text">
+                  <a href="https://www.youtube.com/embed/${vid.id.videoId}"
+                    ><h3>${vid.snippet.title}</h3></a
+                  ><small>By <span> ${vid.snippet.channelTitle} </span> on ${vid.snippet.publishedAt}</small>
+                  <p>${vid.snippet.description}</p>
+                </div>
+              </section>
+              `;
+
+        var buttons = getButtons(prevPageToken, nextPageToken);
+
+        // Display Buttons
+        $('.456').append(buttons);
+      });
+    })
+};
+
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken) {
+  if (!prevPageToken) {
+    var btnoutput = '<div class="button-container">' + '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+      'onclick="nextPage();">Next Page</button></div>';
+  } else {
+    var btnoutput = '<div class="button-container">' +
+      '<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '"' +
+      'onclick="prevPage();">Prev Page</button>' +
+      '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+      'onclick="nextPage();">Next Page</button></div>';
+  }
+
+  return btnoutput;
 }
