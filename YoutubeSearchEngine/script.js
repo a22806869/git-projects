6508b157a5f1be97bc8a5a87773ce6bb2601da8c
chieh <a22806869@hotmail.com.tw>
2021-02-06 23:33:26 +0800

    fix typing error , finish comment, clear code(get rid of console.log), and project finished

--- YoutubeSearchEngine/script.js
+++ YoutubeSearchEngine/script.js
@@ -1,16 +1,23 @@
-//取得input的值並且輸入getVideoData(q)內
+// 功能:取得input的值並且輸入getVideoData(q)內
 $("#btngetval").click(function (e) {
   e.preventDefault();
   let q = $("#inputval").val();
   getVideoData(q);
 });
 
+// 功能:呈現搜尋內容
+
+// 把輸入的資料來搜尋影片使用$.ajax()
+// 網址中的參數是輸入的內容成功載入資料後會跑func sucess
+// 處理得到的資料並且forEach到DOM上面
+// 取出當前網址的下一頁/上一頁的token並且賦予變數並且傳到getBtn裡面
+// 把取得到頁面token以及DOM內容分別貼上HTML
+// iframe 的使用格式得參照官網 頗複雜
 function getVideoData(q) {
   $.ajax({
     type: "GET",
     url: `
     https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&order=relevance&q=${q}&type=video&videoCaption=any&videoEmbeddable=any&videoLicense=any&videoType=any&prettyPrint=true&key=AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I`,
-    data: "data",
     dataType: "json",
     success: function (res) {
       let videos = res.items;
@@ -36,17 +43,22 @@ function getVideoData(q) {
               `;
 
         //貼上搜尋到的結果
-        $(".123").html(output);
+        $(".results").html(output);
 
         // // 加入下一頁按鈕
         let btn = getBtn(nextPageToken, prevPageToken);
-        $(".456").html(btn);
+        $(".buttons").html(btn);
       });
     },
   });
 }
 
-// Next Page Function
+// 功能:呈現下一頁內容
+
+// 功能跟getVideoData()類似
+// 從getBtn()取得當前的q以及nextPageToken來跑下一個頁面的資料出來
+// 清空最初的搜尋內容並且forEach下一頁的內容上去DOM
+// 一樣得取出當前網址的下一頁/上一頁的token並且賦予變數並且傳到getBtn裡面(因為每個下一頁都可能會有上一頁/下一頁)
 function nextPage() {
   let nextPageToken = $("#next-button").data("token");
   let q = $("#next-button").data("query");
@@ -54,14 +66,13 @@ function nextPage() {
   q = $("#inputval").val();
 
   // 清空內容
-  $(".123").html("");
-  $(".456").html("");
+  $(".results").html("");
+  $(".buttons").html("");
 
   $.ajax({
     type: "GET",
     url: `
     https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&order=relevance&pageToken=${nextPageToken}&q=${q}&type=video&videoCaption=any&videoEmbeddable=any&videoLicense=any&videoType=any&prettyPrint=true&key=AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I`,
-    data: "data",
     dataType: "json",
     success: function (data) {
       let nextPageToken = data.nextPageToken;
@@ -92,33 +103,32 @@ function nextPage() {
         let btn = getBtn(nextPageToken, prevPageToken);
 
         //貼上下一頁的搜尋結果
-        $(".123").html(output);
+        $(".results").html(output);
 
         // Display Buttons
-        $(".456").css("display", "flex");
-        $(".456").html(btn);
+        $(".buttons").html(btn);
       });
     },
   });
 }
 
-// Prev Page Function
+// 功能:呈現上一頁內容
+
+// 基本上跟netxPage()功能一樣指示修改參數名稱
 function prevPage() {
-  let token = $("#prev-button").data("token");
+  let prevPageToken = $("#prev-button").data("token");
   let q = $("#next-button").data("query");
 
   q = $("#inputval").val();
 
-  // Clear Results
-  $(".123").html("");
-  $(".456").html("");
+  // 清空內容
+  $(".results").html("");
+  $(".buttons").html("");
 
-  // Run GET Request on API
   $.ajax({
     type: "GET",
     url: `
-    https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&order=relevance&pageToken=${nextPageToken}&q=${q}&type=video&videoCaption=any&videoEmbeddable=any&videoLicense=any&videoType=any&prettyPrint=true&key=AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I`,
-    data: "data",
+    https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&order=relevance&pageToken=${prevPageToken}&q=${q}&type=video&videoCaption=any&videoEmbeddable=any&videoLicense=any&videoType=any&prettyPrint=true&key=AIzaSyDNdqNoZCYqxEJ0nHKh3BWO7Yxc7fLLH2I`,
     dataType: "json",
     success: function (data) {
       let nextPageToken = data.nextPageToken;
@@ -147,43 +157,39 @@ function prevPage() {
         let btn = getBtn(nextPageToken, prevPageToken);
 
         //貼上上一頁的搜尋結果
-        $(".123").html(output);
+        $(".results").html(output);
 
         //貼上按鈕
-        $(".456").css("display", "flex");
-        $(".456").html(btn);
+        $(".buttons").html(btn);
       });
     },
   });
 }
 
-//製造按鈕
+// 功能:製造按鈕
+
+// 按鈕傳遞上一頁/下一頁的token
+// 使用判斷是來決定呈現什麼按鈕
+// 一樣必須定義q是搜尋欄輸入的內容
+// 使用onclick把換頁以及搜尋參數傳入nextPage(),prevPage() function內
 function getBtn(nextPageToken, prevPageToken) {
   let q = $("#inputval").val();
 
   // 如果沒有下一頁則按鈕不顯示
   if (nextPageToken === undefined) {
-    $(".456").html();
+    $(".buttons").html();
     return;
     // 如果沒有上一頁的token則只顯示下一頁
   } else if (prevPageToken === undefined) {
-    $(".456").html(
+    $(".buttons").html(
       `<div class="button-container"><button id="next-button" class="btn paging-button" data-token="${nextPageToken}" data-query="${q}" onclick="nextPage();">Next Page</button></div>`
     );
     return;
     //其他都顯示
   } else {
-    $(".456").html(
+    $(".buttons").html(
       `<div class="button-container"><button id="prev-button" class="btn paging-button" data-token="${prevPageToken}" data-query="${q}" onclick="prevPage();">Prev Page</button></div>
     <div class="button-container"><button id="next-button" class="btn paging-button" data-token="${nextPageToken}" data-query="${q}" onclick="nextPage();">Next Page</button></div> `
     );
   }
 }
-
-// $('[data-fancybox]').fancybox({
-//   toolbar: false,
-//   smallBtn: true,
-//   iframe: {
-//     preload: false
-//   }
-// })
