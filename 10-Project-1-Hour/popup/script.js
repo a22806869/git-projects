c714ad26500b4fdcf9f0ea87a7a1867fae2b8667
chieh <a22806869@hotmail.com.tw>
2021-02-18 20:31:32 +0800

    rewrite css popup-container part

--- 10-Project-1-Hour/popup/script.js
+++ 10-Project-1-Hour/popup/script.js
@@ -2,7 +2,6 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.getElementById('container');
 
-
 open.addEventListener('click', () => {
     container.classList.add('active');
 });
