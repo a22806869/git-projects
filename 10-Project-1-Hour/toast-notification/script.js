d8c13c2e459910d94cf1d0e6d9a05a3960839a5e
chieh <a22806869@hotmail.com.tw>
2021-02-18 19:19:57 +0800

    finish js part produced notification and set it disappear in 3sec

--- 10-Project-1-Hour/toast-notification/script.js
+++ 10-Project-1-Hour/toast-notification/script.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+
 
 btn.addEventListener('click', () => {
     createNotifiction();
@@ -9,6 +11,10 @@ function createNotifiction() {
     const notif = document.createElement('div');
 
     notif.classList.add('toast');
+    container.appendChild(notif);
 
-
+    notif.innerText = 'this challenge is crazy'
+    setTimeout(() => {
+        notif.remove();
+    }, 3000)
 };
