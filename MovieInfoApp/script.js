95bf1c282731d9320435c73956c1074287646ffd
chieh <a22806869@hotmail.com.tw>
2021-02-23 00:56:20 +0800

    js part finished

--- MovieInfoApp/script.js
+++ MovieInfoApp/script.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
 function getMovies(searchText) {
     $.ajax({
         type: "GET",
-        url: `http://www.omdbapi.com/?s=${searchText}&i=tt3896198&apikey=c8fa2a97`,
+        url: `http://www.omdbapi.com/?s=${searchText}&apikey=c8fa2a97`,
         dataType: "json",
         success: function (res) {
             console.log(res);
@@ -37,3 +37,57 @@ function getMovies(searchText) {
         }
     })
 };
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    $.ajax({
+        type: "GET",
+        url: `http://www.omdbapi.com/?i=${movieId}&apikey=c8fa2a97`,
+        dataType: "json",
+        success: function (res) {
+            let movie = res;
+            console.log(movie);
+            let output = `
+                <div class="row">
+                  <div class="col-md-4">
+                    <img src="${movie.Poster}" class="thumbnail">
+                  </div>
+                  <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                    <ul class="list-group">
+                      <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                      <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                      <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                      <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                      <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                      <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                      <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                  </div>
+                </div>
+              `;
+
+            $('#movie').html(output);
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    })
+}
