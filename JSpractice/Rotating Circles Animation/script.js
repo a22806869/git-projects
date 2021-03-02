8c9ae7142554bf4389ac23a7b741904adb260178
chieh <a22806869@hotmail.com.tw>
2021-03-03 02:31:49 +0800

    rotating circle animaiotn project finished

---
+++ JSpractice/Rotating Circles Animation/script.js
@@ -0,0 +1,23 @@
+const circles = document.querySelectorAll('.circle');
+const style = document.createElement('style');
+let styleInner = '';
+
+circles.forEach((circle, idx) => {
+    circle.style.borderWidth = (idx + 1) * 10 + 'px';
+    circle.style.zIndex = -idx;
+    circle.style.animationName = `rotate-${idx}`;
+
+    const deg = (idx + 1) * 360;
+
+    styleInner += `
+		@keyframes rotate-${idx} {
+			to {
+				transform: translate(-50%, -50%) rotate(${deg}deg);
+			}
+		}
+	`;
+});
+
+style.innerHTML = styleInner;
+document.body.appendChild(style);
+console.log(style);
