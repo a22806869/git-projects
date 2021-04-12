e253afcedf579cbb32c0015d24e52a1d32bce0ab
chieh <a22806869@hotmail.com.tw>
2021-04-12 19:28:53 +0800

    start new course about dynamic Components

--- Vue.js-Ninja/playlist/vuejs-playist/src/App.vue
+++ Vue.js-Ninja/playlist/vuejs-playist/src/App.vue
@@ -1,23 +1,13 @@
 <template>
 <div>
-  -helper>
-    <div slot="form-header">
-      <h3>This is the title of the form</h3>
-      <p>information about the form</p>
-    </div>
-    <div slot="form-fields">
-      <input type="text" placeholder="name" required>
-      <input type="password"placeholder="password" required>
-    </div>
-    <div slot="form-controls">
-      <button v-on:click="handleSubmit">Submit</button>
-    </div>
-  </form-helper>
+  
 </div>
 </template>
 
 <script>
-import formHelper from'./components/formHelper.vue'
+import formOne from'./components/formOne.vue'
+import formTwo from'./components/formTwo.vue'
+
 
 
   export default{
